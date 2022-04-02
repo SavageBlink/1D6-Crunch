5a4a33e300876ff126e6d6374f1c1600465d6df4
Yioy <98415535+Yioy@users.noreply.github.com>
2022-04-02 10:03:59 +0200

    Parsing Metric Map first version
    Return data from .txt file

---
+++ mmparsing.py
@@ -0,0 +1,38 @@
+
+def Parsing():
+    #Initialisation variables
+    i = 0;
+    Coordonnee = [[], []]
+    Coor = 0
+    noeud = [[], []]
+    distance = []
+    vitesse = []
+    pente = []
+    consommation = []
+    #open the consommation file
+    with open("consommation_jeu_de_donnee.txt", 'r') as f:
+        for line in f:
+            #Récupère la vaaleur de la ligne
+            Value = line.strip()            #correspond aux valeurs présentent dans le bloc note
+            #Si Value n'est pas une ligne vide
+            if Value != "":
+                #Si Value contient "Arcs"
+                if "Arcs" in Value:
+                    Coor = 1;
+                    continue
+                if Coor == 0:
+                    # Valeur.split(caractère de séparation, nombre de séparation)[indice]
+                    Coordonnee[0].append(Value.split(";", 3)[1])
+                    Coordonnee[1].append(Value.split(";", 3)[2])
+                else :
+                    noeud[0].append(Value.split(";", 5)[0])
+                    noeud[1].append(Value.split(";", 5)[1])
+                    distance.append(Value.split(";", 5)[2])
+                    vitesse.append(Value.split(";", 5)[3])
+                    pente.append(Value.split(";", 5)[4])
+                    consommation.append(Value.split(";", 5)[5])
+        f.close()
+#    for y in range(0, 25000):
+#        print(distance[y])
+    return Coordonnee, noeud, distance, vitesse, consommation
+
